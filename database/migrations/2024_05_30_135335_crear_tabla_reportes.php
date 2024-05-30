a4749b70658c993ddfb4bec492e37ab6e4b3d3f4
Sebastian Rodriguez Camilo <segarodriguezcamilo@gmail.com>
2024-05-30 08:57:17 -0500

    Migration reportes

---
+++ database/migrations/2024_05_30_135335_crear_tabla_reportes.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('reportes', function (Blueprint $table) {
+          $table->id();
+            $table->string('nombre');
+            $table->date('fecha');
+            $table->integer('isla');
+            $table->enum('estado', ['inicio', 'en progreso', 'completo'])->default('inicio');
+            $table->string('archivo')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('reportes');
+    }
+};
